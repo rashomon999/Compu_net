668ea0f9978606904e94b4fbae8f7f659920298d
rashomon999 <luis.gluis.g.io.com@gmail.com>
2025-11-27 07:33:42 -0500

    update

--- project/cliente-web/js/config.js
+++
@@ -1,8 +0,0 @@
-// ============================================
-// js/config.js
-// ============================================
-export const API_URL = "http://localhost:5000";
-export const POLLING_INTERVAL = 3000; // 3 segundos
-
-
- 
